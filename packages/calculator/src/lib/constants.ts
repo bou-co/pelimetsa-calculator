bf88ad57efdbd2217b983250ddf58358e41f4370
Richard GrosJean <richard.grosjean@gmail.com>
2023-05-26 14:42:04 +0300

    fix: kWh standardization and missing k

--- packages/calculator/src/lib/constants.ts
+++ packages/calculator/src/lib/constants.ts
@@ -1,12 +1,12 @@
 //tCO2 factors
-export const usaPowerGrid_tCO2PerKWh = 0.000403;
+export const usaPowerGrid_tCO2_kWh = 0.000403;
 export const singleRountripAverageEmissions = 0.505857142857143;
 export const tCO2PerEmployee = 0.761;
 
 //energy consumption variables
 export const pcEnergyConsumption_kWh = 0.35;
 export const consoleEnergyConsumption_kWh = 0.245;
-export const phoneChargerPower_kW = 0.0425;
+export const phoneChargerPower_kWh = 0.0425;
 export const phoneRechargeTimeInHours = 2.19;
 export const mobileCO2tFactorPerHour = 0.142857;
 
